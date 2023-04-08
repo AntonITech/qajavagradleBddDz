7f28d73c07dfc151b3d341f4deb52130d66ac71d
AntonI <avmiass@bk.ru>
2023-04-08 12:23:47 +0300

    add tests

---
+++ src/test/java/ru/netology/test/MonyTransferNoPageObjectTest.java
@@ -0,0 +1,58 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.data.DataHelper;
+import ru.netology.page.DashboardPage;
+import ru.netology.page.LoginPageV3;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static ru.netology.data.DataHelper.*;
+
+public class MonyTransferNoPageObjectTest {
+    LoginPageV3 loginPage;
+    DashboardPage dashboardPage;
+
+    @BeforeEach
+    void setup() {
+        loginPage = open("http://localhost:9999", LoginPageV3.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCode();
+        dashboardPage = verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        open("http://localhost:9999");
+
+        $("[data-test-id=login] input").setValue("vasya");
+        $("[data-test-id=password] input").setValue("qwerty123");
+        $("[data-test-id=action-login]").click();
+
+        $("[data-test-id=code] input").setValue("12345");
+        $("[data-test-id=action-verify]").click();
+
+        $("[data-test-id=dashboard]").shouldBe(Condition.visible);
+    }
+
+    @Test
+    void shouldTransferFromFirstToSecond() {
+        var firstCardInfo = getFirstCardInfo();
+        var secondCardInfo = getSecondCardInfo();
+        var firstCardBalance = dashboardPage.getCardBalance(firstCardInfo);
+        var secondCardBalance = dashboardPage.getCardBalance(secondCardInfo);
+        var amount = generateValidAmount(firstCardBalance);
+        var expectedBalanceFirstCard = firstCardBalance - amount;
+        var expectedBalanceSecondCard = secondCardBalance + amount;
+        var transferPage = dashboardPage.selectCardToTransfer(secondCardInfo);
+        dashboardPage = transferPage.makeValidTransfer(String.valueOf(amount), firstCardInfo);
+        var actualBalanceFirstCard = dashboardPage.getCardBalance(firstCardInfo);
+        var actualBalanceSecondCard = dashboardPage.getCardBalance(secondCardInfo);
+        assertEquals(expectedBalanceFirstCard, actualBalanceFirstCard);
+        assertEquals(expectedBalanceSecondCard, actualBalanceSecondCard);
+    }
+}
